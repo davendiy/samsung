c74ca980053fb1161208374a27e7f568513f91d8
davidas <davendiy@gmail.com>
2019-01-28 14:33:03 +0200

    add comments

--- task2/main.py
+++ task2/main.py
@@ -53,6 +53,7 @@ InvSbox = (0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9
 
 InvSbox = np.array(InvSbox)    # using array in order to call InvSbox[np.ndarray]
 
+# Hamming weight for numbers from 0 to 255
 hw = [bin(n).count("1") for n in range(0, 256)]
 
 
@@ -67,30 +68,26 @@ def cor2(x, y):
     meanY = np.mean(y)
     x = x - meanX
     y = y - meanY
-    # cov = sum([(x[_i] - meanX) * (y[_i] - meanY) for _i in range(len(x))])
     cov = sum(x*y)
-    # meanQuadraticX = sum([(x[_i] - meanX) ** 2 for _i in range(len(x))]) ** 0.5
     meanQuadraticX = sum(x ** 2)
-    # meanQuadraticY = sum([(y[_i] - meanY) ** 2 for _i in range(len(y))]) ** 0.5
     meanQuadraticY = sum(y ** 2)
-
     res = cov / np.sqrt(meanQuadraticX * meanQuadraticY)
     return abs(res)
 
 
 def cross_all_bytes(texts_out, byte_num, n):
-    """ Byte search function.
+    """ Byte search function (iterator).
     For each byte from 0 to 255 builds array of leakages for all traces in the context of
-    our math model
+    our math model (attack on the last round).
 
     :param texts_out: numpy array of cipher text
     :param byte_num: a number of the byte we search
+    :param n: amount of traces
     :return: supposed byte (int number), array of leakages
     """
     for hyp in range(256):
         leak_array = np.zeros(n)
         for _i in range(n):
-            # leak_array[_i] = hamming_weight(Sbox[texts_in[_i][byte_num]] ^ hyp)
             leak_array[_i] = hw[InvSbox[texts_out[_i][byte_num] ^ hyp]]
         yield hyp, leak_array
 
@@ -103,42 +100,41 @@ def clean(tr: np.ndarray, _b, _a):
     :param _a: result of signal.butter
     :return: clear traces
     """
-    for i in range(len(tr)):
-        tr[i] = signal.lfilter(_b, _a, tr[i])
+    for _i in range(len(tr)):
+        tr[_i] = signal.lfilter(_b, _a, tr[_i])
     return tr
 
 
+# load dataset
 textIn = np.load('textin.npy')
 textOut = np.load('textout.npy')
 traces = np.load('traces.npy')
 
+# clean traces using numpy functions
 b, a = signal.butter(6, 0.65, btype='low')
 traces = clean(traces, b, a)
 
 
 traces_amount = len(textIn)
 meas_amount = len(traces[0])
-
-tmp_traces = np.transpose(traces)
+tmp_traces = np.transpose(traces)    # addition transposed array of traces
 
 plt.plot(traces[0], linewidth=0.2)
 plt.show()
 
-
-key = []
-
-leak_places = []
+key = []             # array of best bytes
+leak_places = []     # array of the moments of time where the best bytes was found
 max_cors = []
+
 for k in range(16):
-    max_cor = 0
-    best_byte = 0
-    leak_place = 0
+    max_cor = 0      # max correlation for each byte
+    best_byte = 0    # the value of the best byte
+    leak_place = 0   # moment of time where the best byte was found
     for byte, leak in cross_all_bytes(textOut, k, traces_amount):
 
-        for i in range(meas_amount):
-            real_values = tmp_traces[i]
-            tmp_cor = cor2(leak, real_values)
-            # print(tmp_cor)
+        for i in range(meas_amount):           # for each byte we search the biggest correlation
+            real_values = tmp_traces[i]        # array of real values of the power consumption
+            tmp_cor = cor2(leak, real_values)  # leak - array of supposed values of the power consumption
             if tmp_cor > max_cor:
                 max_cor = tmp_cor
                 best_byte = byte
@@ -148,7 +144,7 @@ for k in range(16):
     leak_places.append(leak_place)
     key.append(best_byte)
     max_cors.append(max_cor)
-    print('\nbyte number:', k)
+    print('\nbyte number:', k)                # print the information
     print('best byte:', best_byte)
     print('max correlation:', max_cor)
     print('leak place:', leak_places[-1])
@@ -157,5 +153,7 @@ print(key)
 print(leak_places)
 print(max_cors)
 
+
+# write temporary results to the text file
 with open('results.txt', 'a') as file:
     file.write(f'\n{key}\n{leak_places}\n{max_cors}')
